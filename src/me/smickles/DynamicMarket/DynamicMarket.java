2618bdd77d72d594bd053a6469c64df12f51197b
smickles <xiberphim@gmail.com>
2011-10-05 18:10:34 -0400

    Fix IncompatibleClassChangeError, move to CraftBukkit 1240, new version number.

--- src/me/smickles/DynamicMarket/DynamicMarket.java
+++ src/me/smickles/DynamicMarket/DynamicMarket.java
@@ -11,15 +11,16 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.Event.Priority;
+import org.bukkit.event.Event.Type;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.util.config.Configuration;
 
-import com.nijikokun.register.payment.Methods;
+import com.nijikokun.register.payment.Method;
 import com.nijikokun.register.payment.Method.MethodAccount;
-
-import me.smickles.DynamicMarket.Invoice;
 
 public class DynamicMarket extends JavaPlugin {
 
@@ -29,6 +30,7 @@ public class DynamicMarket extends JavaPlugin {
 	public static BigDecimal MINVALUE = BigDecimal.valueOf(.01).setScale(2);
 	public static BigDecimal MAXVALUE = BigDecimal.valueOf(10000).setScale(2);
 	public static BigDecimal CHANGERATE = BigDecimal.valueOf(.01).setScale(2);
+	public Method method;
 	
 	@Override
 	public void onDisable(){
@@ -39,7 +41,7 @@ public class DynamicMarket extends JavaPlugin {
 	@Override
 	public void onEnable() {
 		PluginDescriptionFile pdfFile = this.getDescription();
-		this.logger.info(pdfFile.getName() + " version " + pdfFile.getVersion() + " enabled");
+
 		
 		//item 'config'
 		items = getConfiguration();
@@ -67,6 +69,13 @@ public class DynamicMarket extends JavaPlugin {
 		}
 		
 		items.save();
+		
+		PluginManager pm = this.getServer().getPluginManager();
+		
+		pm.registerEvent(Type.PLUGIN_ENABLE, new DMRegister(this), Priority.Monitor, this);
+		pm.registerEvent(Type.PLUGIN_DISABLE, new DMRegister(this), Priority.Monitor, this);
+		
+		this.logger.info(pdfFile.getName() + " version " + pdfFile.getVersion() + " enabled");
 	}
 	
 
@@ -146,7 +155,7 @@ public class DynamicMarket extends JavaPlugin {
 		if(id != 0) {
 			// determine what it will cost 
 			Invoice invoice = generateInvoice(1, item, amount);
-			MethodAccount cash = Methods.getMethod().getAccount(player.getName());
+			MethodAccount cash = method.getAccount(player.getName());
 			if(cash.hasEnough(invoice.getTotal().doubleValue())) {
 				ItemStack its = new ItemStack(id,amount);
 				player.getInventory().addItem(its);
@@ -212,7 +221,7 @@ public class DynamicMarket extends JavaPlugin {
 		if(id != 0) {
 			// determine what it will pay 
 			Invoice invoice = generateInvoice(0, item, amount);
-			MethodAccount cash = Methods.getMethod().getAccount(player.getName());
+			MethodAccount cash = method.getAccount(player.getName());
 			// If the player has enough of the item, perform the transaction.	
 			if (player.getInventory().contains(id, amount)) {
 				// Figure out how much is left over.
@@ -455,7 +464,7 @@ public class DynamicMarket extends JavaPlugin {
 					// remove the item(s)
 					player.getInventory().removeItem(slot);
 					// "pay the man"
-					MethodAccount cash = Methods.getMethod().getAccount(player.getName());
+					MethodAccount cash = method.getAccount(player.getName());
 					cash.add(thisSale.getTotal().doubleValue());
 					// give nice output
 					player.sendMessage(ChatColor.GREEN + "Sold " + ChatColor.WHITE + slotAmount + " " + ChatColor.GRAY + names.get(x) + ChatColor.GREEN + " for " + ChatColor.WHITE + thisSale.getTotal());
