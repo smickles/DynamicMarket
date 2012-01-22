14a97f952ad03aae5b7098a3e73b0c112bd94771
Michael smickles Carver <xiberphim@gmail.com>
2012-01-21 21:45:44 -0500

    add addCommodity method

--- src/me/smickles/DynamicMarket/CommodityDBAdder.java
+++ src/me/smickles/DynamicMarket/CommodityDBAdder.java
@@ -1,13 +1,23 @@
 package me.smickles.DynamicMarket;
 
-public class CommodityDBAdder {
+public class CommodityDBAdder extends DynamicMarket{
 
 	public CommodityDBAdder() {
 		// TODO Auto-generated constructor stub
 	}
 
-	public void addCommodity(Commodities commodity) {
-		// TODO Auto-generated method stub
+	public void addCommodity(Commodities commodity) throws DuplicateCommodityException {
+		
+    	Commodities check = getDatabase().find(Commodities.class)
+    			.where()
+    			.ieq("number", String.valueOf(commodity.getNumber()))
+    			.ieq("data", String.valueOf(commodity.getData()))
+    			.findUnique();
+    	
+    	if (check != null)
+    		throw new DuplicateCommodityException("Commodity is already in the database");
+    	
+    	getDatabase().save(commodity);
 		
 	}
 
