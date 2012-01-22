44e38cb4b3b0e63cff13f031a68cf23dc0619e82
Michael smickles Carver <xiberphim@gmail.com>
2012-01-21 22:34:00 -0500

    create CommandHelper

---
+++ src/me/smickles/DynamicMarket/CommandHelper.java
@@ -0,0 +1,22 @@
+package me.smickles.DynamicMarket;
+
+import org.bukkit.command.CommandSender;
+
+public class CommandHelper extends DynamicMarket{
+
+	String message;
+	CommandSender sender;
+
+	public CommandHelper(String message, CommandSender sender) {
+
+		super();
+		this.message = message;
+		this.sender = sender;
+	}
+
+	public Object marketAddHelp() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
