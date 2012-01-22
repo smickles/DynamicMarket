739c4a45af21ddd5aad154f6bcd4719a6f442b06
Michael smickles Carver <xiberphim@gmail.com>
2012-01-21 20:56:19 -0500

    Create CommodityDBAdder

---
+++ src/me/smickles/DynamicMarket/CommodityDBAdder.java
@@ -0,0 +1,14 @@
+package me.smickles.DynamicMarket;
+
+public class CommodityDBAdder {
+
+	public CommodityDBAdder() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void addCommodity(Commodities commodity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
